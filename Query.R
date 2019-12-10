f229378fd0885fa2f1457eb47692538debf0e5ad
Katja Nordhoff <contralateral@gmx.net>
2019-12-10 11:27:49 +0100

    Edited query

--- Query.R
+++ Query.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+library(gapminder)
 # Query ----
 # Query	getDeltaLE(name = NULL) 	
 # Given a country, return the increase in years of life expectancy between 1952 and 2007.
@@ -5,11 +7,11 @@
 getDeltaLE <- function(gap_country){
   
   exp2007 <- gapminder %>% 
-    filter(country == gap_country, year == 2007) %>% 
-    select(lifeExp)
+    dplyr::filter(country == gap_country, year == 2007) %>% 
+    dplyr::select(lifeExp)
   exp1952 <- gapminder %>% 
-    filter(country == gap_country, year == 1952) %>% 
-    select(lifeExp)
+    dplyr::filter(country == gap_country, year == 1952) %>% 
+    dplyr::select(lifeExp)
   return(exp2007-exp1952)
 }
 
