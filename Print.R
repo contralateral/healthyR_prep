b5615cd3b5aa5676f298859aa3531d7b00438fe1
Katja Nordhoff <contralateral@gmx.net>
2019-12-10 11:40:01 +0100

    edited library call

--- Print.R
+++ Print.R
@@ -1,11 +1,5 @@
-
-glue("The life expectancy of {gap_country} in {gap_year} was {gapminder %>% 
-     filter(country == gap_country, year == gap_year) %>% 
-     select(lifeExp)} years, copmpared to the average for all countries, {gapminder %>% 
-     filter(year == gap_year) %>% 
-     summarise(avg = mean(lifeExp))}.")
-
-# Turn it into a function
+library(tidyverse)
+library(glue)
 
 printLE <- function(gap_country, gap_year){
   
