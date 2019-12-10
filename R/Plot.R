27a963174a976547aee1532a39ad8f174c315233
Martin Zestermann <martin.zestermann@gmail.com>
2019-12-10 11:51:47 +0100

    Plot updated as function

--- R/Plot.R
+++ R/Plot.R
@@ -3,30 +3,35 @@ library(gapminder)
 library(tidyverse)
 
 data("gapminder")
-gap_D <- gapminder %>% filter(country == "Germany")
+
 
 # Test as factor
-gapminder %>% ggplot(aes(x = log10(gdpPercap), y = lifeExp, size = pop))+
-  geom_point(alpha = .25)+
-  geom_point(data = gap_D, aes(color = as.factor(year)))+
-  scale_colour_brewer(palette = "Dark2")
-
-# Test gradient not working cos of discrete
-gapminder %>% ggplot(aes(x = log10(gdpPercap), y = lifeExp, size = pop))+
-  geom_point(alpha = .25)+
-  geom_point(data = gap_D, aes(color = as.factor(year)))+
-  scale_colour_gradient(low = "red", high = "brown")
+# gapminder %>% ggplot(aes(x = log10(gdpPercap), y = lifeExp, size = pop))+
+#   geom_point(alpha = .25)+
+#   geom_point(data = gap_D, aes(color = as.factor(year)))+
+#   scale_colour_brewer(palette = "Dark2")
+# 
+# # Test gradient not working cos of discrete
+# gapminder %>% ggplot(aes(x = log10(gdpPercap), y = lifeExp, size = pop))+
+#   geom_point(alpha = .25)+
+#   geom_point(data = gap_D, aes(color = as.factor(year)))+
+#   scale_colour_gradient(low = "red", high = "brown")
 
 # work in progress
+plotCountry <- function(name) {
+  
+  gap_country <- gapminder %>% filter(country == name)
+  
 gapminder %>% ggplot(aes(x = log10(gdpPercap), y = lifeExp, size = pop))+
   geom_point(alpha = .25)+
-  geom_point(data = gap_D, aes(color = year))
-  #scale_colour_continuous()
+  geom_point(data = gap_country, aes(color = year))
+  
+}
+
+plotCountry("Germany")
 
 
-
-
-gap_D %>% ggplot(aes(x = log10(gdpPercap), y = lifeExp, size = pop, color = year))+
-  geom_point(alpha = .25)+
+# gap_D %>% ggplot(aes(x = log10(gdpPercap), y = lifeExp, size = pop, color = year))+
+  # geom_point(alpha = .25)+
   
   
