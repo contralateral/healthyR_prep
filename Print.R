4a61d2471f30afc7407fc1981cd23e1be7cc5cba
Martin Zestermann <martin.zestermann@gmail.com>
2019-12-10 11:40:56 +0100

    ugly version

--- Print.R
+++ Print.R
@@ -13,3 +13,13 @@ printLE <- function(gap_country, gap_year){
 printLE("Afghanistan", 2007)
 
 printLE("Afghanistan", 1952)
+
+
+# Sentence
+query="Germany"
+query2="1952"
+#gapminder[gapminder$country==query,gapminder$year==query2]
+gapminder %>% filter(country==query, year==query2) ->lifeExpQ 
+gapminder %>%  summarise(avg=mean(lifeExp)) -> avg_LifeExp
+
+glue('The life expectancy of {query} in {query2} was {lifeExpQ$lifeExp} years, compared to the average for all countries, {avg_LifeExp}.This is the xxxth percentile.')
