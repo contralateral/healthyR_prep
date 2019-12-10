40240de7ba98c655e8a30dcbeb03c60afc6fedfb
Katja Nordhoff <contralateral@gmx.net>
2019-12-10 11:36:42 +0100

    added comments

--- Plot.R
+++ Plot.R
@@ -1,27 +1,27 @@
 # Plot ----
 library(gapminder)
+library(tidyverse)
+
 data("gapminder")
 gap_D <- gapminder %>% filter(country == "Germany")
 
-gapminder %>% ggplot(aes(x = log10(gdpPercap), y = lifeExp, size = pop))+
-  geom_point(alpha = .25)+
-  ggplot(gap_D, aes(x = log10(gdpPercap), y = lifeExp, size = pop, colour = year))+
-  geom_point()
-
+# Test as factor
 gapminder %>% ggplot(aes(x = log10(gdpPercap), y = lifeExp, size = pop))+
   geom_point(alpha = .25)+
   geom_point(data = gap_D, aes(color = as.factor(year)))+
   scale_colour_brewer(palette = "Dark2")
 
+# Test gradient not working cos of discrete
 gapminder %>% ggplot(aes(x = log10(gdpPercap), y = lifeExp, size = pop))+
   geom_point(alpha = .25)+
   geom_point(data = gap_D, aes(color = as.factor(year)))+
   scale_colour_gradient(low = "red", high = "brown")
 
+# work in progress
 gapminder %>% ggplot(aes(x = log10(gdpPercap), y = lifeExp, size = pop))+
   geom_point(alpha = .25)+
-  geom_point(data = gap_D, aes(color = year))+
-  scale_colour_continuous()
+  geom_point(data = gap_D, aes(color = year))
+  #scale_colour_continuous()
 
 
 
